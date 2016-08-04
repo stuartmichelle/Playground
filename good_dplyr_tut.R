9e4a88ea8ad5781bf7a57e1a168aeeab7d7eab66
Michelle Stuart <bunx21@yahoo.com>
2016-08-03 21:05:26 -0400

    updated

--- good_dplyr_tut.R
+++ good_dplyr_tut.R
@@ -18,7 +18,7 @@ flights <- tbl_df(hflights)
 flights
 
 # you can specify that you want to see more rows (example doesn't show more)
-print(flights, )
+print(flights, n=20)
 
 # convert to a normal data frame to see all of the columns
 data.frame(head(flights))
@@ -99,122 +99,124 @@ flights %>%
   arrange(DepDelay)
 
 # use `desc` for descending
-flights %%
-select(UniqueCarrier, DepDelay) %%
-arrange(desc(DepDelay))
+flights %>%
+  select(UniqueCarrier, DepDelay) %>%
+  arrange(desc(DepDelay))
 
-mutate: Add new variables
-
-- Create new variables that are functions of existing variables
+# mutate: Add new variables
+# 
+# - Create new variables that are functions of existing variables
 
 # base R approach to create a new variable Speed (in mph)
-flightsSpeed <- flightsDistance  flightsAirTime
-flights[, c(Distance, AirTime, Speed)]
+flights$Speed <- flights$Distance / flights$AirTime*60
+flights[, c("Distance", "AirTime", "Speed")]
 
 # dplyr approach (prints the new variable but does not store it)
-flights %%
-select(Distance, AirTime) %%
-mutate(Speed  DistanceAirTime)
+flights %>%
+  select(Distance, AirTime) %>%
+  mutate(Speed = Distance/AirTime*60)
 
 # store the new variable
-flights <- flights %% mutate(Speed  DistanceAirTime)
+flights <- flights %>% mutate(Speed = Distance/AirTime*60)
 
-summarise: Reduce variables to values
-
-- Primarily useful with data that has been grouped by one or more variables
-- group_by creates the groups that will be operated on
-- summarise uses the provided aggregation function to summarise each group
+# summarise: Reduce variables to values
+# 
+# - Primarily useful with data that has been grouped by one or more variables
+# - group_by creates the groups that will be operated on
+# - summarise uses the provided aggregation function to summarise each group
 
 # base R approaches to calculate the average arrival delay to each destination
-head(with(flights, tapply(ArrDelay, , , na.rm)))
-head(aggregate(ArrDelay  , flights, ))
+head(with(flights, tapply(ArrDelay, Dest, mean, na.rm=TRUE)))
+head(aggregate(ArrDelay ~ Dest, flights, mean))
 
 # dplyr approach: create a table grouped by Dest, and then summarise each group by taking the mean of ArrDelay
-flights %%
-group_by() %%
-summarise(avg_delay  mean(ArrDelay, na.rm))
+flights %>%
+  group_by(Dest) %>%
+  summarise(avg_delay = mean(ArrDelay, na.rm=TRUE))
 
-- summarise_each allows you to apply the same summary function to multiple columns at once
-- Note: mutate_each is also available
+# - summarise_each allows you to apply the same summary function to multiple columns at once
+# - Note: mutate_each is also available
 
 # for each carrier, calculate the percentage of flights cancelled or diverted
-flights %%
-group_by(UniqueCarrier) %%
-summarise_each(funs(), Cancelled, Diverted)
+flights %>%
+  group_by(UniqueCarrier) %>%
+  summarise_each(funs(mean), Cancelled, Diverted)
 
 # for each carrier, calculate the minimum and maximum arrival and departure delays
-flights %%
-group_by(UniqueCarrier) %%
-summarise_each(funs(min(., na.rm), max(., na.rm)), matches(Delay))
+flights %>%
+  group_by(UniqueCarrier) %>%
+  summarise_each(funs(min(., na.rm=TRUE), max(., na.rm=TRUE)), matches("Delay"))
 
-- Helper function n() counts the number of rows in a group
-- Helper function n_distinct(vector) counts the number of unique items in that vector
+# - Helper function n() counts the number of rows in a group
+# - Helper function n_distinct(vector) counts the number of unique items in that vector
 
 # for each day of the year, count the total number of flights and sort in descending order
-flights %%
-group_by(Month, DayofMonth) %%
-summarise(flight_count  n()) %%
-arrange(desc(flight_count))
+flights %>%
+  group_by(Month, DayofMonth) %>%
+  summarise(flight_count = n()) %>%
+  arrange(desc(flight_count))
 
 # rewrite more simply with the `tally` function
-flights %%
-group_by(Month, DayofMonth) %%
-tally(  )
+flights %>%
+  group_by(Month, DayofMonth) %>%
+  tally(sort = TRUE)
+
 
 # for each destination, count the total number of flights and the number of distinct planes that flew there
-flights %%
-group_by() %%
-summarise(flight_count  n(), plane_count  n_distinct(TailNum))
+flights %>%
+  group_by(Dest) %>%
+  summarise(flight_count = n(), plane_count = n_distinct(TailNum))
 
-- Grouping can sometimes be useful without summarising
+# Grouping can sometimes be useful without summarising
 
 # for each destination, show the number of cancelled and not cancelled flights
-flights %%
-group_by() %%
-select(Cancelled) %%
-table() %%
-head()
+flights %>%
+  group_by(Dest) %>%
+  select(Cancelled) %>%
+  table() %>%
+  head()
 
-Window Functions
-
-- Aggregation function (like mean) takes n inputs and returns 1 value
-- Window function takes n inputs and returns n values
-- Includes ranking and ordering functions (like min_rank), offset functions (lead and lag), and cumulative aggregates (like cummean).
+# Window Functions
+# 
+# - Aggregation function (like mean) takes n inputs and returns 1 value
+# - Window function takes n inputs and returns n values
+# - Includes ranking and ordering functions (like min_rank), offset functions (lead and lag), and cumulative aggregates (like cummean).
 
 # for each carrier, calculate which two days of the year they had their longest departure delays
 # note: smallest (not largest) value is ranked as 1, so you have to use `desc` to rank by largest value
-flights %%
-group_by(UniqueCarrier) %%
-select(Month, DayofMonth, DepDelay) %%
-filter(min_rank(desc(DepDelay))  ) %%
-arrange(UniqueCarrier, desc(DepDelay))
+flights %>%
+  group_by(UniqueCarrier) %>%
+  select(Month, DayofMonth, DepDelay) %>%
+  filter(min_rank(desc(DepDelay)) <= 2) %>%
+  arrange(UniqueCarrier, desc(DepDelay))
 
 # rewrite more simply with the `top_n` function
-flights %%
-group_by(UniqueCarrier) %%
-select(Month, DayofMonth, DepDelay) %%
-top_n() %%
-arrange(UniqueCarrier, desc(DepDelay))
+flights %>%
+  group_by(UniqueCarrier) %>%
+  select(Month, DayofMonth, DepDelay) %>%
+  top_n(2) %>%
+  arrange(UniqueCarrier, desc(DepDelay))
 
 # for each month, calculate the number of flights and the change from the previous month
-flights %%
-group_by(Month) %%
-summarise(flight_count  n()) %%
-mutate(change  flight_count  lag(flight_count))
+flights %>%
+  group_by(Month) %>%
+  summarise(flight_count = n()) %>%
+  mutate(change = flight_count - lag(flight_count))
 
 # rewrite more simply with the `tally` function
-flights %%
-group_by(Month) %%
-tally() %%
-mutate(change    lag())
+flights %>%
+  group_by(Month) %>%
+  tally() %>%
+  mutate(change = n - lag(n))
 
-Other Useful Convenience Functions
+# Other Useful Convenience Functions
 
 # randomly sample a fixed number of rows, without replacement
-flights %% sample_n()
+flights %>% sample_n(5)
 
-# randomly sample a fraction of rows, with replacement
-flights %% sample_frac(, replace)
+# randomly sample a fraction of rows, with replacement 
+# (what does replacement mean?)
+flights %>% sample_frac(0.25, replace=TRUE)
 
 # base R approach to view the structure of an object
 str(flights)
@@ -222,49 +224,49 @@ str(flights)
 # dplyr approach: better formatting, and adapts to your screen width
 glimpse(flights)
 
-Connecting to Databases
-
-- dplyr can connect to a database as if the data was loaded into a data frame
-- Use the same syntax for local data frames and databases
-- Only generates SELECT statements
-- Currently supports SQLite, PostgreSQL/Redshift, MySQL/MariaDB, BigQuery, MonetDB
-- Example below is based upon an SQLite database containing the hflights data
-- Instructions for creating this database are in the databases vignette
+# Connecting to Databases
+# 
+# - dplyr can connect to a database as if the data was loaded into a data frame
+# - Use the same syntax for local data frames and databases
+# - Only generates SELECT statements
+# - Currently supports SQLite, PostgreSQL/Redshift, MySQL/MariaDB, BigQuery, MonetDB
+# - Example below is based upon an SQLite database containing the hflights data
+# - Instructions for creating this database are in the databases vignette
 
 # connect to an SQLite database containing the hflights data
-my_db <- src_sqlite(my_db.sqlite3)
+my_db <- src_sqlite("my_db.sqlite3")
 
 # connect to the "hflights" table in that database
-flights_tbl <- tbl(my_db, hflights)
+flights_tbl <- tbl(my_db, "hflights")
 
 # example query with our data frame
-flights %%
-select(UniqueCarrier, DepDelay) %%
-arrange(desc(DepDelay))
+flights %>%
+  select(UniqueCarrier, DepDelay) %>%
+  arrange(desc(DepDelay))
 
 # identical query using the database
-flights_tbl %%
-select(UniqueCarrier, DepDelay) %%
-arrange(desc(DepDelay))
+flights_tbl %>%
+  select(UniqueCarrier, DepDelay) %>%
+  arrange(desc(DepDelay))
 
-- You can write the SQL commands yourself
-- dplyr can tell you the SQL it plans to run and the query execution plan
+# - You can write the SQL commands yourself
+# - dplyr can tell you the SQL it plans to run and the query execution plan
 
 # send SQL commands to the database
-tbl(my_db, sql(SELECT * FROM hflights LIMIT 100))
+tbl(my_db, sql("SELECT * FROM hflights LIMIT 100"))
 
 # ask dplyr for the SQL commands
-flights_tbl %%
-select(UniqueCarrier, DepDelay) %%
-arrange(desc(DepDelay)) %%
-explain()
+flights_tbl %>%
+  select(UniqueCarrier, DepDelay) %>%
+  arrange(desc(DepDelay)) %>%
+  explain()
 
-Resources
-
-- Official dplyr reference manual and vignettes on CRAN: vignettes are well-written and cover many aspects of dplyr
-- July 2014 webinar about dplyr (and ggvis) by Hadley Wickham and related slides/code: mostly conceptual, with a bit of code
-- dplyr tutorial by Hadley Wickham at the useR! 2014 conference: excellent, in-depth tutorial with lots of example code (Dropbox link includes slides, code files, and data files)
-- dplyr GitHub repo and list of releases
-
-< END OF DOCUMENT >
+# Resources
+# 
+# - Official dplyr reference manual and vignettes on CRAN: vignettes are well-written and cover many aspects of dplyr
+# - July 2014 webinar about dplyr (and ggvis) by Hadley Wickham and related slides/code: mostly conceptual, with a bit of code
+# - dplyr tutorial by Hadley Wickham at the useR! 2014 conference: excellent, in-depth tutorial with lots of example code (Dropbox link includes slides, code files, and data files)
+# - dplyr GitHub repo and list of releases
+# 
+# < END OF DOCUMENT >
 
