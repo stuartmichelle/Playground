a6c30d021b568a0142d6c49281be7eb5b27ea74f
Michelle Stuart <bunx21@yahoo.com>
2016-10-06 13:16:57 -0400

    initial commit

---
+++ Chris's R Class/lab1_r_basics.R
@@ -0,0 +1,110 @@
+
+# In this script, we'll learn the basics of R!
+
+# R as a calculator
+################################################################################
+
+# R can perform ALL mathematical operations
+# +, -, *, /, ^, exp(), sqrt(), log(), log10(), abs(), and more
+
+
+
+
+# R is logical
+################################################################################
+
+# R can perform ALL logical tests
+# <, <=, >, >=, ==, !=, 
+# ! = NOT, | = OR, & = AND, %in% = in
+
+
+
+
+
+# R uses objects 
+################################################################################
+
+# Create simple objects
+################################
+
+# Creating simple objects
+# Create objects using the <- operator
+# Objects can be numeric, integer, string, complex, logical, and more
+
+
+
+
+
+# Create vectors
+################################
+
+# Creating numerical vectors
+# You can create numeric vectors using c(), seq(), or the : operator
+
+
+
+
+# Creating character (string vectors)
+# You can create character vectors using c() and paste()
+
+
+
+
+# Combining vectors
+# You can combine vectors using c()
+
+
+
+
+# What happens when you combine a string and character vector?
+
+
+
+
+# Indexing vectors 
+################################
+
+# Indexing vectors
+# Use []s to index a vector - put the element position in the bracked
+
+
+
+
+
+# EXERCISE BREAK!
+################################################################################
+
+# Basic math
+################################
+
+# 1. What is the sum of 856 + 765?
+
+
+# 2. What is 104 multiplied by 187
+
+
+# 3. What is 1 divided by 12 + 47
+
+
+#  Vectors
+################################
+
+# 4. Create a vector of 1 to 5, save it as object x
+
+
+# 5. Create a vector of 12, 17, 9, 11, save it as object y
+
+
+# 6. Create a vector of both x and y together
+
+
+# 7. Run g<-seq(11,333,3), what is the 68 position in this vector?
+
+
+
+# 8. How do you call both the 68 position and 79 simultaneously?
+
+
+
+
+
---
+++ Chris's R Class/lab1_using_data_in_r.R
@@ -0,0 +1,100 @@
+
+# In this script, we'll learn to read, manipulate, and plot data!
+
+# Read data
+##########################################
+
+# Read CSV file using read.csv()
+
+
+
+# Inspect data
+##########################################
+
+# Inspect data
+# str(), head(), tail(), nrow(), ncol(), colnames()
+
+
+
+# Reduce data to important columns 
+# ID, year, water body, species, age, length, weight, sex, egg count
+
+
+
+
+# Inspect data again
+# Inspect species names
+
+
+
+# Subset data with []'s and $'s
+
+
+
+
+# Subset data with subset()
+# Some common logic statements: 
+# ==, !=, >=, <=, >, <, %in%, is.na(), !is.na(), &, |
+
+
+
+
+
+# EXERCISE BREAK
+##########################################
+
+# Subset burbot data with both length and weight measurements
+# using []s and $s and using subset()
+
+
+
+
+# Subset a vector of egg counts for Hovsgol grayling (grayling-Hovsgol)
+# Hint: You can use na.omit() to eliminate NA values
+
+
+
+
+# Summarize data
+##########################################
+
+# Summary statistics
+# sum(), mean(), median(), quantile(), summary(), boxplot()
+
+
+
+
+
+# Add columns to data
+##########################################
+
+# Add total length in cm to data (tl_cm)
+
+
+
+
+# Export data
+##########################################
+
+# Export cleaned data using write.csv()
+
+
+
+# Plot data
+##########################################
+
+# Plot Hovsgol grayling length and weight
+
+
+# On your own, try to make as pretty a plot as possible!
+# Search online about the different arguments used in the plot function
+
+
+
+
+
+
+
+
+
+
